0058ba6b47530eb8b53e04315acb31c613fbd2bb
MarceloRarz <marcelorarz@outlook.com>
2018-05-07 14:01:49 -0300

    Ate Aula  27

--- exercicios_react_marcelo/ex/component.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Primeiro = props => (
-    <h1>Primeiro Compoenete!</h1>
-)
-
-const Segundo = props => <h1>Segundo Componente!</h1>
-
-export { Primeiro, Segundo}
